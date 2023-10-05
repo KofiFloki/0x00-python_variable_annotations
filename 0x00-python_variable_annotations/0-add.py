b6d4728cc25ccb5c40d06d728691ea05f1e820f2
KofiFloki <Jeremiah.martey@yahoo.com>
2023-10-05 16:55:00 +0000

    Basic annotations - concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,11 +3,9 @@
 This is a module that provides a function for adding two numbers.
 """
 
-
 def add(a: float, b: float) -> float:
     """
     This function adds two numbers and returns the result.
-
 
     Parameters:
     a (float): The first number.
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,8 +1,17 @@
 #!usr/bin/env python3
-'''Task 1's module.
+'''This is a module that provides a function for concatenating two strings.
 '''
 
-def concat(str1: str, str2: str -> str :
-'''concatenates 2 strings'''
 
-    return str1 + str2;
+def concat(str1: str, str2: str) -> str:
+    '''
+    This function concatenates two strings and returns the result.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenation of str1 and str2.
+    '''
+    return str1 + str2
