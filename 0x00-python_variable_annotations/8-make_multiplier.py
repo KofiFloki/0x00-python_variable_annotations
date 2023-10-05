f44a5528d18ee8565997c2fb4f5becae43c32336
KofiFloki <Jeremiah.martey@yahoo.com>
2023-10-05 22:44:39 +0000

    Complex types - string and int/float to tuple

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 This is a module that provides a function for creating a function that
-multiplies a float by a multiplier.
+    multiplies a float by a multiplier.
 """
 from typing import Callable
 
@@ -20,17 +20,16 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
 
 
-def multiply(n: float) -> float:
-    """
-    This function multiplies a float by the multiplier.
+    def multiply(n: float) -> float:
+        """
+        This function multiplies a float by the multiplier.
 
-    Parameters:
-    n (float): The float to multiply by the multiplier.
+        Parameters:
+        n (float): The float to multiply by the multiplier.
 
-    Returns:
-    float: The result of multiplying n by the multiplier.
-    """
-    return n * multiplier
+        Returns:
+        float: The result of multiplying n by the multiplier.
+        """
+        return n * multiplier
 
-
-return multiply
+    return multiply
