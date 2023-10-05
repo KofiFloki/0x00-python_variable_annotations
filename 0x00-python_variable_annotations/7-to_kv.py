645c8d44b531d2a81e0d98cf06aa69654d3e5492
KofiFloki <Jeremiah.martey@yahoo.com>
2023-10-05 22:40:54 +0000

    Complex types - string and int/float to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 """
-This module provides variables with type annotations.
-
-The variables are:
-    - a: an integer with the value 1
-    - pi: a float with the value 3.14
-    - i_understand_annotations: a boolean with the value True
-    - school: a string with the value 'Holberton'
+This is a module that provides a function for creating a tuple with a string
+    and the square of an int or float as a float
 """
+from typing import Union, Tuple
 
-a: int = 1
-pi: float = 3.14
-i_understand_annotations: bool = True
-school: str = 'Holberton'i
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    This function takes a string and an int or float, and returns a tuple with
+        the string and the square of the int or float as a float.
+
+    Parameters:
+    k (str): The string to include in the tuple.
+    v (Union[int, float]): The int or float to square.
+
+    Returns:
+    Tuple[str, float]: A tuple with the string k and the square of v as a
+    float.
+    """
+    return (k, float(v ** 2))
