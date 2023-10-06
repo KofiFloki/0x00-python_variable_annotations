b7b62147a9414979a7452c5aac20aaaea3c12775
KofiFloki <Jeremiah.martey@yahoo.com>
2023-10-06 04:57:05 +0000

    Define variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -4,10 +4,10 @@ This module provides variables with type annotations.
 
 
 The variables are:
-    - a: an integer with the value 1
-    - pi: a float with the value 3.14
-    - i_understand_annotations: a boolean with the value True
-    - school: a string with the value 'Holberton'
+- a: an integer with the value 1
+- pi: a float with the value 3.14
+- i_understand_annotations: a boolean with the value True
+- school: a string with the value 'Holberton'
 """
 
 a: int = 1
